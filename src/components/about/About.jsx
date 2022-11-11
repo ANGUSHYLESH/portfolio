f48f538bde5bb6cca4e3f47ee3254afc76820788
Shylu <sshylesh34@gmail.com>
2022-11-11 16:15:22 +0530

    .

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  return <div>About</div>;
+};
 
-export default About
+export default About;
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./nav.css";
+
+const Nav = () => {
+  return <div></div>;
+};
+
+export default Nav;
